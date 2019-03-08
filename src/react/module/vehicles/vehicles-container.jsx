4f9f1023b557091232d067d50a2ea86bddd0bbc0
Donna <donnai@rigbot.com>
2019-03-08 16:48:13 +0530

    Create vehicles container

---
+++ src/react/module/vehicles/vehicles-container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class VehiclesContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Vehicles Container</h4>
+            </div>
+        );
+    }
+}
+
+export default VehiclesContainer;
