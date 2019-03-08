2cd2d274aa817663b278313103bea15097a1c511
Donna <donnai@rigbot.com>
2019-03-08 16:48:03 +0530

    Create time container

---
+++ src/react/module/time/time-container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TimeContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Time Container</h4>
+            </div>
+        );
+    }
+}
+
+export default TimeContainer;
