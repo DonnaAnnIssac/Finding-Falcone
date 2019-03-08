0cc78b6dcd25bda60fa2662e47c6b1c3637c6ae2
Donna <donnai@rigbot.com>
2019-03-08 16:47:54 +0530

    Create planets container

---
+++ src/react/module/planets/planets-container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class PlanetsContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h4>Planet Container</h4>
+            </div>
+        );
+    }
+}
+
+export default PlanetsContainer;
