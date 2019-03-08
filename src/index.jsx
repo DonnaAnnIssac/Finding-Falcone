742864075710cd6f6344005e090b3e176b4cd880
Donna <donnai@rigbot.com>
2019-03-08 14:58:42 +0530

    Add react router module

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { browserHistory, Router } from "react-router";
+import routes from "./react/config/routes";
+// import App from "./react/app";
 
-import App from "./react/app";
-
-ReactDOM.render(<App/>, document.getElementById("root"));
+ReactDOM.render(<Router history={browserHistory}>{routes}</Router>, document.getElementById("root"));
