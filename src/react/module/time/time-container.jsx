8deb41db32431947dd8a9e02da1ed5e68787abd7
Donna <donnai@rigbot.com>
2019-03-13 16:11:24 +0530

    Display time taken given the selected planet's distance and selected vehicle's speed

--- src/react/module/time/time-container.jsx
+++ src/react/module/time/time-container.jsx
@@ -3,10 +3,17 @@ import React from "react";
 class TimeContainer extends React.Component {
     render() {
         return (
-            <div>
-                <h4>Time Container</h4>
+            <div className="timeContainer">
+                <h4>Time Taken</h4>
+                {this.calculateTimeTaken()}
             </div>
         );
+    }
+
+    calculateTimeTaken() {
+        return this.props.selectedPlanet && this.props.selectedVehicle
+        ? this.props.selectedPlanet.distance / this.props.selectedVehicle.speed
+        : 0;
     }
 }
 
