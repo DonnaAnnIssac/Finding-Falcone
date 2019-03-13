f7a8feb5f3f2d43c834933d2ced5c9dca97cfc0f
Donna <donnai@rigbot.com>
2019-03-13 16:10:07 +0530

    Create container that holds all planets and pass container props as props to children

--- src/react/module/planets/planets-container.jsx
+++ src/react/module/planets/planets-container.jsx
@@ -1,10 +1,25 @@
+import "babel-polyfill";
 import React from "react";
+import Planet from "./planet";
 
 class PlanetsContainer extends React.Component {
     render() {
         return (
-            <div>
-                <h4>Planet Container</h4>
+            <div className="planetsContainer">
+                {/* <h4>Planet Container</h4> */}
+                <div>
+                    {this.props.planetsArray.map((planetObj, index) => {
+                        return (
+                            <Planet
+                                planetData={planetObj}
+                                selected={this.props.selectedPlanet && this.props.selectedPlanet.name === planetObj.name
+                                    ? true : false}
+                                onClickHandler={this.props.onClick}
+                                key={index}
+                            />
+                        )
+                    })}
+                </div>
             </div>
         );
     }
