c8081af7d376d0ec4904bb180eae98250f3f321e
Donna <donnai@rigbot.com>
2019-03-13 16:07:48 +0530

    Fetch planets & vehicles array, save  arrays and selected planet/vehicle in state, add on click handlers for planets & vehicles, pass state vars and handlers as props

--- src/react/main-container.jsx
+++ src/react/main-container.jsx
@@ -3,16 +3,78 @@ import PlanetsContainer from "./module/planets/planets-container"
 import VehiclesContainer from "./module/vehicles/vehicles-container"
 import TimeContainer from "./module/time/time-container";
 
-class MainContainer extends React.Component{
+class MainContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      planets : [],
+      selectedPlanet: null,
+      vehicles : [],
+      selectedVehicle: null,
+    }
+    this.onPlanetRowClick = this.onPlanetRowClick.bind(this);
+    this.onVehicleRowClick = this.onVehicleRowClick.bind(this);
+  }
   render(){
     return(
       <div className="mainContainer">
-        <h3> Hello, World! </h3>
-        <PlanetsContainer />
-        <VehiclesContainer />
-        <TimeContainer />
+        <PlanetsContainer
+          planetsArray={this.state.planets}
+          selectedPlanet={this.state.selectedPlanet}
+          onClick={this.onPlanetRowClick}
+        />
+        <VehiclesContainer
+          display={this.state.selectedPlanet ? true : false}
+          selectedVehicle={this.state.selectedVehicle}
+          selectedPlanet={this.state.selectedPlanet}
+          vehiclesArray={this.state.vehicles}
+          onClick={this.onVehicleRowClick}
+        />
+        <TimeContainer selectedPlanet={this.state.selectedPlanet} selectedVehicle={this.state.selectedVehicle}/>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.fetchPlanets();
+    this.fetchVehicles();
+  }
+
+  async fetchPlanets() {
+    const planetsInfo = await fetch("https://findfalcone.herokuapp.com/planets");
+    const planetsInfoJson = await planetsInfo.json();
+    // console.log(planetsInfoJson);
+    this.setState({
+        planets: planetsInfoJson,
+    })
+  }
+
+  async fetchVehicles() {
+    const vehiclesInfo = await fetch("https://findfalcone.herokuapp.com/vehicles");
+    const vehiclesInfoJson = await vehiclesInfo.json();
+    // console.log(vehiclesInfoJson);
+    this.setState({
+        vehicles : vehiclesInfoJson,
+    })
+  }
+
+  onPlanetRowClick(planetName) {
+    const planet = this.state.planets.find(planet => planet.name === planetName)
+    this.setState({
+        selectedPlanet: planet,
+        selectedVehicle: null,
+    }, () => {
+        console.log(this.state.selectedPlanet);
+    })
+  }
+
+  onVehicleRowClick(vehicleName) {
+    const vehicle = this.state.vehicles.find(vehicle => vehicle.name === vehicleName)
+    this.setState({
+      selectedVehicle: vehicle
+    }, () => {
+      console.log(this.state.selectedVehicle);
+    })
   }
 }
 
