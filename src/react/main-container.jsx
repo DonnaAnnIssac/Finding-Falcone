71bc23d5b37d583870e91d5a86a4f530b72cc743
Donna <donnai@rigbot.com>
2019-03-14 04:39:34 +0530

    Define click handler to add planet to chosen list, pass it as props to time container, if 4 planets chosen render results

--- src/react/main-container.jsx
+++ src/react/main-container.jsx
@@ -11,32 +11,45 @@ class MainContainer extends React.Component {
       selectedPlanet: null,
       vehicles : [],
       selectedVehicle: null,
+      chosenPlanets: 0,
+      findFalcone: false,
+      totalTime: 0,
     }
     this.onPlanetRowClick = this.onPlanetRowClick.bind(this);
     this.onVehicleRowClick = this.onVehicleRowClick.bind(this);
+    this.onButtonClick = this.onButtonClick.bind(this);
   }
-  render(){
-    return(
+
+  render() {
+    if (!this.state.findFalcone) {
+      return (
+        <div className="mainContainer">
+          <PlanetsContainer
+            planetsArray={this.state.planets}
+            selectedPlanet={this.state.selectedPlanet}
+            onClick={this.onPlanetRowClick}
+          />
+          <VehiclesContainer
+            display={this.state.selectedPlanet ? true : false}
+            selectedVehicle={this.state.selectedVehicle}
+            selectedPlanet={this.state.selectedPlanet}
+            vehiclesArray={this.state.vehicles}
+            onClick={this.onVehicleRowClick}
+          />
+          <TimeContainer
+            display={this.state.selectedPlanet && this.state.selectedVehicle ? true : false}
+            selectedPlanet={this.state.selectedPlanet}
+            selectedVehicle={this.state.selectedVehicle}
+            onClick={this.onButtonClick}
+          />
+        </div>
+      );
+    }
+    return (
       <div className="mainContainer">
-        <PlanetsContainer
-          planetsArray={this.state.planets}
-          selectedPlanet={this.state.selectedPlanet}
-          onClick={this.onPlanetRowClick}
-        />
-        <VehiclesContainer
-          display={this.state.selectedPlanet ? true : false}
-          selectedVehicle={this.state.selectedVehicle}
-          selectedPlanet={this.state.selectedPlanet}
-          vehiclesArray={this.state.vehicles}
-          onClick={this.onVehicleRowClick}
-        />
-        <TimeContainer
-          display={this.state.selectedPlanet && this.state.selectedVehicle ? true : false}
-          selectedPlanet={this.state.selectedPlanet}
-          selectedVehicle={this.state.selectedVehicle}
-        />
+        <ResultContainer/>
       </div>
-    );
+    )
   }
 
   componentDidMount() {
@@ -73,6 +86,36 @@ class MainContainer extends React.Component {
     this.setState({
       selectedVehicle: vehicle
     });
+  }
+
+  onButtonClick() {
+    let count = 0;
+    // disable selected planet from list
+    // check if four planets have been chosen
+    const tempPlanets = this.state.planets;
+    tempPlanets.forEach((planet) => {
+      if (planet.chosen) {
+        count ++;
+      }
+      if (planet.name === this.state.selectedPlanet.name) {
+        planet.chosen = true;
+        count ++;
+      }
+    });
+    const tempVehicles = this.state.vehicles;
+    tempVehicles.forEach((vehicle) => {
+      if (vehicle.name === this.state.selectedVehicle.name) {
+        vehicle.total_no = vehicle.total_no - 1;
+      }
+    })
+    // set state selectedPlanet and selectedVehicle to null
+    this.setState({
+      selectedPlanet : null,
+      selectedVehicle : null,
+      planets : tempPlanets,
+      vehicles: tempVehicles,
+      findFalcone: count === 4 ? true : false,
+    })
   }
 }
 
