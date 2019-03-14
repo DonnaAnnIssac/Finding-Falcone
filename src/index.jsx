d6c5537c191f5ffe78ad9da8e889fa6b02dbd466
Donna <donnai@rigbot.com>
2019-03-14 19:25:11 +0530

    Pass App component to render method

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { browserHistory, Router } from "react-router";
-import routes from "./react/config/routes";
-// import App from "./react/app";
+import App from "./react/app";
 
-ReactDOM.render(<Router history={browserHistory}>{routes}</Router>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
