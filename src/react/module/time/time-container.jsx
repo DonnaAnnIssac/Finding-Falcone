95dccb3c3e835c2f96250b4e168aad8e455ca74e
Donna <donnai@rigbot.com>
2019-03-14 19:27:50 +0530

    Display time taken for each planet - vehicle combo and also total time taken with chosen planets

--- src/react/module/time/time-container.jsx
+++ src/react/module/time/time-container.jsx
@@ -1,28 +1,48 @@
 import React from "react";
 
 class TimeContainer extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            timeTaken: 0,
+            totalTime: 0
+        }
+    }
     render() {
-        const timeTakenDiv = this.props.display
-            ? 
-                <div>
-                    <h4>Time Taken</h4>
-                    {this.calculateTimeTaken()}
+        if (this.props.display) {
+            return (
+                <div className="timeContainer">
                     <div>
-                        <button onClick={(e) => this.onClick(e)}>Select</button>
+                        <h4>Time Taken</h4>
+                        {this.state.timeTaken}
+                        <div>
+                            <button onClick={(e) => this.onClick(e)}>Select</button>
+                        </div>
+                        <h4>Total Time</h4>
+                        {this.state.totalTime}
                     </div>
                 </div>
-            : ""
+            )
+        }
         return (
             <div className="timeContainer">
-                {timeTakenDiv}
             </div>
-        );
+        )            
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        this.calculateTimeTaken(nextProps);
     }
 
-    calculateTimeTaken() {
-        return this.props.selectedPlanet && this.props.selectedVehicle
-        ? this.props.selectedPlanet.distance / this.props.selectedVehicle.speed
+    calculateTimeTaken(props) {
+        const time = props.selectedPlanet && props.selectedVehicle
+        ? props.selectedPlanet.distance / props.selectedVehicle.speed
         : 0;
+        const totalTimeTaken = this.state.totalTime;
+        this.setState({
+            timeTaken: time,
+            totalTime: totalTimeTaken + time,
+        });
     }
 
     onClick(e) {
