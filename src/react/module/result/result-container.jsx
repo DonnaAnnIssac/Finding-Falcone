d33cc1f1501980a7c668416004396353d73c285b
Donna <donnai@rigbot.com>
2019-03-14 05:35:06 +0530

    Create result container, fetch token and use token to fetch result and pass result to result component

---
+++ src/react/module/result/result-container.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Result from "./result"
+export default class ResultContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: "",
+            foundInPlanet: "",
+        }
+    }
+    render() {
+        return(
+            <div>
+                Finding Falcone...
+                <Result status={this.state.status} planetName={this.state.foundInPlanet}/>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.fetchResults();
+    }
+
+    async fetchResults() {
+        const token = await fetch("https://findfalcone.herokuapp.com/token", {
+            method: "POST",
+            headers: {
+                "Accept" : "application/json",
+            },
+            body: JSON.stringify({})
+        });
+        const tokenJson = await token.json();
+        const planetNames = this.props.planets.map((planet) => planet.name);
+        const vehicleNames = this.props.vehicles.map((vehicle) => vehicle.name);
+        const result = await fetch("https://findfalcone.herokuapp.com/find", {
+            method: "POST",
+            headers: {
+                "Accept" : "application/json",
+                "Content-Type" : "application/json",
+            },
+            body: JSON.stringify({
+                "token" : tokenJson.token,
+                "planet_names" : planetNames,
+                "vehicle_names" : vehicleNames,
+            })
+        })
+        const resultJson = await result.json();
+        this.setState({
+            status: resultJson.status === "false" ? "Failure" : "Success",
+            foundInPlanet: resultJson.planet_name,
+        })
+    }
+}
