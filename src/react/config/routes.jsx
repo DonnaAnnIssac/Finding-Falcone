a2a7dbdff3670e37022000e2f644e049489b01bd
Donna <donnai@rigbot.com>
2019-03-14 19:24:47 +0530

    Remove use of react reouter for routes

--- src/react/config/routes.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import {Route, Router} from "react-router";
-import App from "../app";
-export default (
-    <Router>
-        <Route path="/" component={App}>
-        </Route>
-    </Router>
-)
