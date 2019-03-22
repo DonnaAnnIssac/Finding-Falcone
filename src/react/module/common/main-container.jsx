bb6541b8745fe2ed40831db79d6301eea0cca4c6
Donna <donnai@rigbot.com>
2019-03-22 18:37:22 +0530

    Remove commented out code

--- src/react/module/common/main-container.jsx
+++ src/react/module/common/main-container.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-// import PlanetsContainer from "../planets/planets-container"
-// import VehiclesContainer from "../vehicles/vehicles-container"
-// import TimeContainer from "../time/time-container";
-// import ResultContainer from "../result/result-container";
 import Main from "./main";
-import About from "./about";
 
 class MainContainer extends React.Component {
   render() {
